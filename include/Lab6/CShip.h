d95f13f539f202180e26f8d31526999f2d9e3c1d
braegen2 <bradenvanderwoerd@gmail.com>
2025-02-14 11:02:36 -0800

    Created CShip shell

--- include/Lab6/CShip.h
+++ include/Lab6/CShip.h
@@ -1,7 +1,14 @@
 #pragma once
+#include "glad.h"
 #include "Lab6/CGameObject.h"
 
 class CShip : public CGameObject {
+	
+	private:
+		GLuint VAO, VBO, EBO;
 
-
+	public:
+		CShip();
+		~CShip();
+		void draw();
 };
--- src/Lab6/CShip.cpp
+++ src/Lab6/CShip.cpp
@@ -1,1 +1,27 @@
+#include <glm/glm.hpp>
 #include "Lab6/CShip.h"
+
+
+CShip::CShip() {
+    std::vector<glm::vec3> shipVertices = {
+            { 0.0f,  0.5f,  0.0f}, { 0.5f, -0.5f,  0.5f},
+            {-0.5f, -0.5f,  0.5f}, { 0.0f, -0.5f, -0.5f}
+    };
+
+    std::vector<unsigned int> shipEdges = {
+            0, 1,  0, 2,  0, 3,  1, 2,  2, 3,  3, 1
+    };
+
+    glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
+
+
+}
+
+CShip::~CShip() {
+
+}
+
+void CShip::draw() {
+
+}
